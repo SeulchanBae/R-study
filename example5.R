a7c01bc9dadb1f205eb6180d36a6e21a4949ce49
chan <slchan5292@gmail.com>
2024-11-17 21:22:08 +0900

    Add untracked files before rebase

---
+++ example5.R
@@ -0,0 +1,10 @@
+# example4.R
+
+# 두 숫자의 곱을 계산하는 함수 정의
+multiply <- function(a, b) {
+    return(a * b)
+}
+
+# 함수 호출
+result <- multiply(5, 3)
+print(paste("The result of multiplication is:", result))
