40a5cd48693c702cf7ac37cc4a620a4c90da2e4b
chan <slchan5292@gmail.com>
2024-11-17 21:21:08 +0900

    example2 add python

---
+++ example2.R
@@ -0,0 +1,19 @@
+# example1.R
+
+# 변수 선언
+x <- 10
+y <- 20
+
+# 두 변수의 합
+result <- x + y
+print(paste("The sum of x and y is:", result))
+
+
+
+# # Python 변수 선언
+# x = 10
+# y = 20
+
+# # 두 변수의 합
+# result = x + y
+# print(f"The sum of x and y is: {result}")
