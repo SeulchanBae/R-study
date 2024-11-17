5537628d6852160a62bcd43eb18e4303e55c88cf
chan <slchan5292@gmail.com>
2024-11-17 21:09:26 +0900

    Add example1.R file

---
+++ example1.R
@@ -0,0 +1,17 @@
+# R: 데이터 프레임 예제
+data <- data.frame(
+    Name = c("Alice", "Bob", "Charlie"),
+    Age = c(25, 30, 35)
+)
+print(data)
+
+
+# # Python: 데이터 프레임 예제
+# import pandas as pd
+
+# data = {
+#     "Name": ["Alice", "Bob", "Charlie"],
+#     "Age": [25, 30, 35]
+# }
+# df = pd.DataFrame(data)
+# print(df)
