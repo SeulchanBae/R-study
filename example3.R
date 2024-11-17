99f69d863c5c38491ba8a5c376a77395817a9cd9
chan <slchan5292@gmail.com>
2024-11-17 20:57:19 +0900

    third

---
+++ example3.R
@@ -0,0 +1,18 @@
+# R: 조건문 예제
+x <- -1
+if (x > 0) {
+    print("Positive")
+} else if (x == 0) {
+    print("Zero")
+} else {
+    print("Negative")
+}
+
+# # Python: 조건문 예제
+# x = -1
+# if x > 0:
+#     print("Positive")
+# elif x == 0:
+#     print("Zero")
+# else:
+#     print("Negative")
